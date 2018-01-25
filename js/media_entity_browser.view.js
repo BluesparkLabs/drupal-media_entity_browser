6c3d7ed5753c9ccd8ddfd1de24d40fae094a703f
Pablo Cerda <pablo@citla.com>
2018-01-25 17:40:22 +0100

    Extended JS behaviors to fix field cardinality handling.
    When a media reference field limits the cardinality the media browser is
    not adapting the JS validation behavior to limit the allowed selected
    items to the constaints configured at the field instance. The
    file_entity_browser module had the same issue where so we ported the
    patch from https://www.drupal.org/project/file_browser/issues/2917823 to
    work on media entity browser and adapted the file counts message to
    display in single references source plugin view and added a limit
    message warning when user reach the maximum selected items.

--- js/media_entity_browser.view.js
+++ js/media_entity_browser.view.js
@@ -3,22 +3,157 @@
  * Defines the behavior of the media entity browser view.
  */
 
-(function ($) {
+(function ($, _, Backbone, Drupal) {
 
   "use strict";
+
+  var Selection = Backbone.View.extend({
+
+    events: {
+      'click .views-row': 'onClick',
+      'dblclick .views-row': 'onClick'
+    },
+
+    // Display selected items counter.
+    renderCounter() {
+      $('.media-browser-file-counter').each(function () {
+        $(this).remove();
+      });
+      var text = Drupal.formatPlural(this.count, 'Selected 1 item.', 'Selected @count items.');
+      var $counter = $('<div class="media-browser-file-counter"></div>').text(text);
+      $('.view-content').prepend($counter);
+    },
+
+    initialize: function () {
+      // This view must be created on an element which has this attribute.
+      // Otherwise, things will blow up and rightfully so.
+      this.uuid = this.el.getAttribute('data-entity-browser-uuid');
+
+      // If we're in an iFrame, reach into the parent window context to get the
+      // settings for this entity browser.
+      var settings = (frameElement ? parent : window).drupalSettings.entity_browser[this.uuid];
+
+      // Assume a single-cardinality field with no existing selection.
+      this.count = settings.count || 0;
+      this.cardinality = settings.cardinality || 1;
+      this.renderCounter();
+    },
+
+    deselect: function (item) {
+      this.$(item)
+        .removeClass('checked')
+        .find('input[name ^= "entity_browser_select"]')
+        .prop('checked', false);
+    },
+
+    /**
+     * Deselects all items in the entity browser.
+     */
+    deselectAll: function () {
+      // Create a version of deselect() that can be called within each() with
+      // this as its context.
+      var _deselect = jQuery.proxy(this.deselect, this);
+
+      this.$('.views-row').each(function (undefined, item) {
+        _deselect(item);
+      });
+    },
+
+    select: function (item) {
+      this.$(item)
+        .addClass('checked')
+        .find('input[name ^= "entity_browser_select"]')
+        .prop('checked', true);
+    },
+
+    /**
+     * Marks unselected items in the entity browser as disabled.
+     */
+    lock: function () {
+      var text = Drupal.t('No more items could be selected, you reached the items limit.');
+      var $message = $('<div class="media-browser-limit messages warning"></div>').text(text);
+      this.$('.view-content').prepend($message);
+      this.$('.views-row:not(.checked)').addClass('disabled');
+    },
+
+    /**
+     * Marks all items in the entity browser as enabled.
+     */
+    unlock: function () {
+      this.$('.media-browser-limit').each(function () {
+        $(this).remove();
+      });
+
+      this.$('.views-row').removeClass('disabled');
+    },
+
+    /**
+     * Handles click events for any item in the entity browser.
+     *
+     * @param {jQuery.Event} event
+     */
+    onClick: function (event) {
+
+      var chosen_one = this.$(event.currentTarget);
+
+      if (chosen_one.hasClass('disabled')) {
+        return false;
+      }
+      else if (this.cardinality === 1) {
+        this.deselectAll();
+        this.select(chosen_one);
+      }
+      else if (chosen_one.hasClass('checked')) {
+        this.deselect(chosen_one);
+        this.count--;
+        this.unlock();
+        this.renderCounter();
+      }
+      else {
+        this.select(chosen_one);
+
+        // If cardinality is unlimited, this will never be fulfilled. Good.
+        if (++this.count === this.cardinality) {
+          this.lock();
+        }
+
+        this.renderCounter();
+      }
+    }
+
+  });
 
   /**
    * Attaches the behavior of the media entity browser view.
    */
   Drupal.behaviors.mediaEntityBrowserView = {
-    attach: function (context, settings) {
-      $('.views-row', context).once().click(function () {
-        var $row = $(this);
-        var $input = $row.find('.views-field-entity-browser-select input');
-        $input.prop('checked', !$input.prop('checked'));
-        $row[$input.prop('checked') ? 'addClass' : 'removeClass']('checked');
-      });
+
+    getElement: function (context) {
+      // If we're in a document context, search for the first available entity
+      // browser form. Otherwise, ensure that the context is itself an entity
+      // browser form.
+      return $(context)[context === document ? 'find' : 'filter']('form[data-entity-browser-uuid]').get(0);
+    },
+
+    attach: function (context) {
+      var element = this.getElement(context);
+      if (element) {
+        $(element).data('view', new Selection({ el: element }));
+      }
+    },
+
+    detach: function (context) {
+      var element = this.getElement(context);
+
+      if (element) {
+        var view = $(element).data('view');
+
+        if (view instanceof Selection) {
+          view.undelegateEvents();
+        }
+      }
     }
+
   };
 
-}(jQuery, Drupal));
+})(jQuery, _, Backbone, Drupal);
